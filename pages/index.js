20c686de2c57c1bb2f1263cefeeedc2b9298b2e4
Klaysson Leite Nogueira <klayssonln@gmail.com>
2024-11-27 00:01:37 +0000

    Mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home(){
-    return <h1>Você meu vidaaa S2</h1>
+    return 
+    <body>
+        <h1>Oi Débora Leite</h1>
+        <p>Meu tudos</p>
+        <p>Minha vida</p>
+        <p>Minha paixão</p>
+        <p>Minha casa, meu lar!</p>
+        <p>Obrigado por hoje e sempre</p>
+        <p>Te amo infinito... e mais um potinho</p>
+
+    </body>
+    
 }
 
 export default Home;
